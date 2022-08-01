47fa79f0c689d28ecb6a94ec9d427ef27cfef53d
Aryan Sharma <aryansharmayea@gmail.com>
2022-08-01 14:10:25 +0530

    Creating Profiles

---
+++ Nurse.java
@@ -0,0 +1,13 @@
+package src;
+
+public class Nurse extends Staff{
+    private long nurseId;
+
+    public void setNurseId(long nurseId){
+        this.nurseId = nurseId;
+    }
+
+    public long getNurseId(){
+        return nurseId;
+    }
+}
